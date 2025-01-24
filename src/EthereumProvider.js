bb5900f53f389ea00ca4173ca067f1dd7dd9032a
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-24 09:20:22 -0500

    Update EthereumProvider.js

--- src/EthereumProvider.js
+++ src/EthereumProvider.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
-import * as PropTypes from 'prop-types'
+'use strict';
 
-import EthereumService from './EthereumService'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import createEthereumService from './EthereumService'; // Adjust the path based on your project structure
+
+class EthereumProvider extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    if (!this.props.web3) {
+      throw new Error("EthereumProvider was not passed a web3 instance. Make sure you pass it in via the 'web3' prop.");
+    }
+
+    this.state = {
+      ethereum: createEthereumService(this.props.web3)
+    };
+  }
+
+  getChildContext() {
+    return {
+      ethereum: this.state.ethereum
+    };
+  }
 
 export default class EthereumProvider extends Component {
 
